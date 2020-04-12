a5d6397a32fe1be35557ba3e930d04f781f134ed
Ryo Aoyama <r.fe51028.r@gmail.com>
2020-04-12 11:40:03 +0900

    Update xcode versions on CI

--- Tools/swift-run.sh
+++ Tools/swift-run.sh
@@ -11,7 +11,9 @@ BIN_DIR=$($SWIFT_BUILD --show-bin-path)
 BIN="$BIN_DIR/$PACKAGE_NAME"
 CHECKSUM="$BIN_DIR/$PACKAGE_NAME.sha256"
 
-if [[ ! -e $BIN || ! -e $CHECKSUM || $($(shasum -a 256 -c $CHECKSUM >/dev/null) echo $0) != 0 ]]; then
+shasum -a 256 -c $CHECKSUM && check=0 || check=1
+
+if [[ ! -e $BIN || $check != 0 ]]; then
     echo "$PACKAGE_NAME is not installed"
     echo "Installing ..."
     $SWIFT_BUILD --product $PACKAGE_NAME
