8f6021f6a3471b139da4b21cee46af07a2da4f2c
Ryo Aoyama <r.fe51028.r@gmail.com>
2020-04-12 11:36:26 +0900

    Fix swift run script

--- Tools/swift-run.sh
+++ Tools/swift-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-set -e
+set -eu
 
-PACKAGE_NAME=${1?"USAGE: swift_run.sh #{package name} #{parameter}"}
-PARAMETERS=${@:2}
-SWIFT_BUILD="swift build -c release --package-path $(dirname $0)"
-PACKAGE_PATH=$($SWIFT_BUILD --show-bin-path)
-CHECKSUM_PATH="$PACKAGE_PATH/$PACKAGE_NAME.sha256"
-PACKAGE="$PACKAGE_PATH/$PACKAGE_NAME"
+ROOT=$(cd $(dirname $0)/.. && pwd)
+PACKAGE_NAME=${1?"USAGE: $(basename $0) <package name> <parameter>"}
+ARGS=${@:2}
+PACKAGE_PATH="$ROOT/Tools"
+SWIFT_BUILD="swift build -c release --package-path $PACKAGE_PATH"
+BIN_DIR=$($SWIFT_BUILD --show-bin-path)
+BIN="$BIN_DIR/$PACKAGE_NAME"
+CHECKSUM="$BIN_DIR/$PACKAGE_NAME.sha256"
 
-if [[ -e $PACKAGE && -e $CHECKSUM_PATH && $(shasum -a 256 -c $CHECKSUM_PATH) ]]; then
-    echo "$PACKAGE_NAME already installed"
-else
-    echo "$PACKAGE_NAME has not been installed"
+if [[ ! -e $BIN || ! -e $CHECKSUM || $($(shasum -a 256 -c $CHECKSUM >/dev/null) echo $0) != 0 ]]; then
+    echo "$PACKAGE_NAME is not installed"
+    echo "Installing ..."
     $SWIFT_BUILD --product $PACKAGE_NAME
-    shasum -a 256 $(dirname $0)/Package.resolved >$CHECKSUM_PATH
-    echo "Build succeeded"
+    shasum -a 256 "$PACKAGE_PATH/Package.resolved" >$CHECKSUM
 fi
 
-$PACKAGE $PARAMETERS
+$BIN $ARGS
