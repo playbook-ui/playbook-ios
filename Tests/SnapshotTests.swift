09e9b1ee266b815c14ca2afdc26721b695cb6301
Ryo Aoyama <r.fe51028.r@gmail.com>
2020-10-31 02:37:32 +0900

    Reduce snapshots

--- Tests/SnapshotTests.swift
+++ Tests/SnapshotTests.swift
@@ -21,30 +21,9 @@ final class SnapshotTests: XCTestCase {
                     .iPhone11Pro(.landscape, style: .dark),
                     .iPhoneSE(.portrait, style: .dark),
                     .iPadPro12_9(.landscape),
-                ]
-            )
-        )
-    }
-
-    func testTakeSnapshotWithPreprocessor() throws {
-        guard let directory = ProcessInfo.processInfo.environment["SNAPSHOT_DIR"] else {
-            fatalError("Set directory to the build environment variables with key `SNAPSHOT_DIR`.")
-        }
-
-        Playbook.default.add(AllScenarios.self)
-
-        try Playbook.default.run(
-            Snapshot(
-                directory: URL(fileURLWithPath: directory).appendingPathComponent("GrayBackground"),
-                clean: true,
-                format: .png,
-                scale: 1,
-                keyWindow: UIApplication.shared.windows.first { $0.isKeyWindow },
-                devices: [
-                    .iPhone11Pro(.portrait)
                 ],
                 viewPreprocessor: { view in
-                    view.backgroundColor = .lightGray
+                    view.backgroundColor = .white
                     return view
                 }
             )
