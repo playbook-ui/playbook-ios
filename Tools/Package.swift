3db443830ba35607b5b26f3e277ec2f770a99f2c
ra1028 <r.fe51028.r@gmail.com>
2021-08-25 19:07:06 +0900

    Update tool versions

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -4,12 +4,8 @@ import PackageDescription
 
 let package = Package(
     name: "Tools",
-    platforms: [.macOS(.v11)],
     dependencies: [
-        .package(url: "https://github.com/ra1028/swift-mod.git", .exact("0.0.4")),
-        .package(url: "https://github.com/apple/swift-format.git", .branch("swift-5.3-branch")),
-        .package(url: "https://github.com/yonaskolb/XcodeGen.git", .exact("2.18.0")),
-        .package(url: "https://github.com/SwiftDocOrg/swift-doc.git", .revision("164157531625a77474884185d1cba0c12ebe0cfa")),
-    ],
-    targets: [.target(name: "Tools", path: "TargetStub")]
+        .package(url: "https://github.com/apple/swift-format.git", .branch("0.50400.0")),
+        .package(url: "https://github.com/yonaskolb/XcodeGen.git", .exact("2.24.0")),
+    ]
 )
