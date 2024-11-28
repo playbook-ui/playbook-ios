ceef5d8529f760e902b18c1fdeb8ac26fca6c1bc
Ras <ras.suleimenov@mercari.com>
2024-11-28 16:31:31 +0900

    Bump dependencies and swift to 5.10

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "dev-tools",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-format.git", exact: "509.0.0"),
-        .package(url: "https://github.com/yonaskolb/XcodeGen.git", exact: "2.40.1"),
+        .package(url: "https://github.com/swiftlang/swift-format.git", exact: "510.1.0"),
+        .package(url: "https://github.com/yonaskolb/XcodeGen.git", exact: "2.42.0"),
     ]
 )
